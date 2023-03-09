63f2aedaa58fc4c30a782f9a3f9c10d707cdfb8c
Elian Santos <eliansds001@gmail.com>
2023-03-08 21:22:24 -0300

    config function e corrigindo id input

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 const form = document.getElementById('form-validacao')
 
+function validaNumero(validacao) {
+    document.getElementById('#n1', '#n2')
+    if( '#n1' < '#n2') return true
+    if( '#n1' > '#n2') return false
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 })
